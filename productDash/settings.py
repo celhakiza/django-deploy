475be2a439355ea2156ffc5ebfb9e506e4c08cd1
HAKIZIMANA Celestin <91734054+celhakiza@users.noreply.github.com>
2025-08-16 16:04:04 +0200

    change settings to collect staticfiles

--- productDash/settings.py
+++ productDash/settings.py
@@ -122,9 +122,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # STATIC_URL = 'static/'
-# STATICFILES_DIRS=[
-#     BASE_DIR/'static'
-# ]
+STATICFILES_DIRS=[BASE_DIR/'static'
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
